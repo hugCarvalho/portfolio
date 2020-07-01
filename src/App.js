6f162fad2405de9b743a5f7d0c7c4eb9f1f08c25
Hugo Carvalho <webnoico1978@gmail.com>
2020-07-01 12:18:58 +0200

    Resets

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      Header Content: Projects Skills About Contact Footer
+    </div>
+  );
+}
+
+export default App;
