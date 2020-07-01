e3c1f2eb93f7329c4850275b1f6ade172e5e3e5a
Hugo Carvalho <webnoico1978@gmail.com>
2020-07-01 12:32:45 +0200

    Very first basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      Header Content: Projects Skills About Contact Footer
+      <Header />
+      <div className="render-pages">Render Pages</div>
+      <Footer />
     </div>
   );
 }
