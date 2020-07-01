abe4f78780886a7880688b5bf7b2a3dae415f930
Hugo Carvalho <webnoico1978@gmail.com>
2020-07-01 13:34:08 +0200

    Set links

--- src/App.js
+++ src/App.js
@@ -2,13 +2,39 @@ import React from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Projects from "./components/Pages/Projects/Projects";
+import Skills from "./components/Pages/Skills/Skills";
+import About from "./components/Pages/About/About";
+import Contact from "./components/Pages/Contact/Contact";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="render-pages">Render Pages</div>
-      <Footer />
+      <Router>
+        <div className="App">
+          <Header />
+
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/projects" />
+            </Route>
+            <Route path="/projects" component={Projects} />
+            <Route exact path="/skills" component={Skills} />
+
+            <Route exact path="/about" component={About} />
+
+            <Route exact path="/contact" component={Contact} />
+            <Route path="/" render={() => <div>Page not found!</div>} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Link to="/projects">projects</Link>
+      <Link to="/projects1">projects1</Link>
+      <Link to="/projects2">projects2</Link>
+      Header
+    </div>
+  );
 }
 
 export default Header;
