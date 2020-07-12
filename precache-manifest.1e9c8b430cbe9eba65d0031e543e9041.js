266ead18eaf4f3f4551b88e2922f1e93ba0cc5d6
Hugo Carvalho <webnoico1978@gmail.com>
2020-07-12 13:26:16 +0200

    Updates

---
+++ precache-manifest.1e9c8b430cbe9eba65d0031e543e9041.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5529deae24ff8ef917cf64fdab9e1e6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a0648670982a1b86735e",
+    "url": "/portfolio/static/css/main.94d81762.chunk.css"
+  },
+  {
+    "revision": "75aad94309a4b7c771c1",
+    "url": "/portfolio/static/js/2.2532ae0f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.2532ae0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0648670982a1b86735e",
+    "url": "/portfolio/static/js/main.564da290.chunk.js"
+  },
+  {
+    "revision": "b821e00807eba5befb0c",
+    "url": "/portfolio/static/js/runtime-main.a844750a.js"
+  },
+  {
+    "revision": "f1aca55b0652269e5a94e63a54e387be",
+    "url": "/portfolio/static/media/cv.f1aca55b.pdf"
+  },
+  {
+    "revision": "1c4499a706921c26b2ce015af2727254",
+    "url": "/portfolio/static/media/flagEngland.1c4499a7.png"
+  },
+  {
+    "revision": "ccde240b6ebf069fe7d4d9d37563e45e",
+    "url": "/portfolio/static/media/flagGermany.ccde240b.png"
+  },
+  {
+    "revision": "73d8e155d4e7811d64eac80ce897f871",
+    "url": "/portfolio/static/media/moi.73d8e155.jpg"
+  }
+]);
