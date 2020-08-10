f66c37582069ef02e26a489161d20614e0937cae
Hugo Carvalho <webnoico1978@gmail.com>
2020-08-10 23:49:52 +0200

    Updates

---
+++ precache-manifest.1d7d158cab5e73bed3a9e3879ba3773c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d89eb14aaa090245e55307471d73102",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "25f20516643aa2fde4d7",
+    "url": "/portfolio/static/css/main.0bf9ac38.chunk.css"
+  },
+  {
+    "revision": "d268a1b15568a64b3fd8",
+    "url": "/portfolio/static/js/2.f7df868b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.f7df868b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f20516643aa2fde4d7",
+    "url": "/portfolio/static/js/main.73a703fb.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "32f342dc213018ec80781d7eee93bd9d",
+    "url": "/portfolio/static/media/CV_deutsch.32f342dc.pdf"
+  },
+  {
+    "revision": "d54452a54afda05c141a171156e3dab3",
+    "url": "/portfolio/static/media/CV_english.d54452a5.pdf"
+  },
+  {
+    "revision": "1c4499a706921c26b2ce015af2727254",
+    "url": "/portfolio/static/media/flagEngland.1c4499a7.png"
+  },
+  {
+    "revision": "ccde240b6ebf069fe7d4d9d37563e45e",
+    "url": "/portfolio/static/media/flagGermany.ccde240b.png"
+  },
+  {
+    "revision": "90bbaa7972bf8e282ae5b71f891f2c34",
+    "url": "/portfolio/static/media/logo_sprites.90bbaa79.png"
+  },
+  {
+    "revision": "295d2fc3a4194ec8064307158a14fa9c",
+    "url": "/portfolio/static/media/moi.295d2fc3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1ab3da1cec3054dd0b2504b098d34007.js"
+  "/portfolio/precache-manifest.1d7d158cab5e73bed3a9e3879ba3773c.js"
 );
 
 self.addEventListener('message', (event) => {
