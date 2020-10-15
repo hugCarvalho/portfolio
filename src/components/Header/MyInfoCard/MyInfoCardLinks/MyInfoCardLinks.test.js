f41af31814b00e4a6b837f226cd35bee9d5e72a5
Hugo Carvalho <webnoico1978@gmail.com>
2020-10-15 17:57:45 +0200

    MyInfoCard: added tests to links

---
+++ src/components/Header/MyInfoCard/MyInfoCardLinks/MyInfoCardLinks.test.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
+import App from "../../../../App";
+import MyInfoCardLinks from "./MyInfoCardLinks";
+
+describe("MyInfoCardLinks", () => {
+  test("button links are present and link to correspondent resources", () => {
+    const Wrapper = ({ children }) => {
+      return (
+        <Router history={history}>
+          <App>{children}</App>
+        </Router>
+      );
+    };
+    const history = createMemoryHistory();
+    render(<MyInfoCardLinks />, { wrapper: Wrapper });
+
+    // present in English
+    const cardLinks = document.querySelector(".MyInfoCardLinks > ul");
+    expect(cardLinks).toHaveTextContent("Contact");
+    expect(cardLinks).toHaveTextContent("CV");
+    expect(cardLinks).toHaveTextContent("GitHub");
+
+    //Same links on whole website link properly, not only the ones on this component
+    const contactLink = screen.getAllByRole("link", { name: /contact/i });
+    contactLink.forEach((link) => {
+      fireEvent.click(link);
+      expect(link.href).toContain("contact");
+      expect(screen.getByRole("button", { name: /contact/i })).toHaveClass("active");
+    });
+
+    const githubLink = screen.getAllByRole("link", { name: /github/i });
+    githubLink.forEach((link) =>
+      expect(link.href).toBe("https://github.com/hugCarvalho")
+    );
+
+    const cvLink = screen.getAllByRole("link", { name: /cv/i });
+    cvLink.forEach((link) => expect(link.href).toContain("CV_english"));
+  });
+
+  test("German: links are present and link to correspondent resources", () => {
+    const Wrapper = ({ children }) => {
+      return (
+        <Router history={history}>
+          <App isEnglish={false}>{children}</App>
+        </Router>
+      );
+    };
+    const history = createMemoryHistory();
+    render(<MyInfoCardLinks />, { wrapper: Wrapper });
+
+    // present in German
+    const cardLinks = document.querySelector(".MyInfoCardLinks > ul");
+    expect(cardLinks).toHaveTextContent("Kontakt");
+    expect(cardLinks).toHaveTextContent("CV");
+    expect(cardLinks).toHaveTextContent("GitHub");
+
+    //Same links on whole website link properly, not only the ones on this component
+    const contactLink = screen.getAllByRole("link", { name: /kontakt/i });
+    contactLink.forEach((link) => {
+      fireEvent.click(link);
+      // expect(link.href).toContain("kontakt");
+      expect(screen.getByRole("button", { name: /kontakt/i })).toHaveClass("active");
+    });
+
+    const githubLink = screen.getAllByRole("link", { name: /github/i });
+    githubLink.forEach((link) =>
+      expect(link.href).toBe("https://github.com/hugCarvalho")
+    );
+
+    const cvLink = screen.getAllByRole("link", { name: /cv/i });
+    cvLink.forEach((link) => expect(link.href).toContain("CV_deutsch"));
+  });
+});
--- src/components/Header/MyInfoCard/RenderMyInfoCard.test.js
+++ src/components/Header/MyInfoCard/RenderMyInfoCard.test.js
@@ -4,10 +4,11 @@ import App from "../../../App";
 import "@testing-library/jest-dom";
 import RenderMyInfoCard from "./RenderMyInfoCard";
 
-describe.skip("Header elements are present", () => {
-  test("MyInfoCard is present in english and german", () => {
-    render(RenderMyInfoCard);
-    screen.getByTestId("My Info Card");
-    screen.getAllByRole("link", { name: /contact/i });
-  });
+test("MyInfoCard is present per default language", () => {
+  render(
+    <App>
+      <RenderMyInfoCard />
+    </App>
+  );
+  screen.getByTestId("My Info Card");
 });
