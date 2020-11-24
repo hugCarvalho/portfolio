325ac97621ce373b3db530ec308a42aa8b28981f
Hugo Carvalho <webnoico1978@gmail.com>
2020-11-24 22:19:54 +0100

    MyInfoCard: added eeffects. Minor button tweak

--- src/components/Header/MyInfoCard/RenderMyInfoCard.js
+++ src/components/Header/MyInfoCard/RenderMyInfoCard.js
@@ -5,8 +5,17 @@ import MyPic from "./MyPic/MyPic";
 import LanguagesSpoken from "./LanguagesSpoken/LanguagesSpoken";
 
 function RenderMyInfoCard() {
+  const [cardStart, setCardStart] = React.useState(false);
+  React.useEffect(() => {
+    setCardStart(true);
+    //return () => setCardStart(false);
+  }, []);
   return (
-    <section className="RenderMyInfoCard" id="mycard" data-testid="My Info Card">
+    <section
+      className={`RenderMyInfoCard ${cardStart && "start"} `}
+      id="mycard"
+      data-testid="My Info Card"
+    >
       <MyInfoCardLinks />
       <MyPic />
       <LanguagesSpoken />
