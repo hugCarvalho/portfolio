6c135b373b3e9548aba7bd4341cadd2ef94d1862
Hugo Carvalho <webnoico1978@gmail.com>
2020-11-25 15:50:19 +0100

    Picture: open left-right effect

--- src/components/Header/MyInfoCard/MyPic/MyPic.js
+++ src/components/Header/MyInfoCard/MyPic/MyPic.js
@@ -6,13 +6,27 @@ import moi from "./media/moi.jpg";
 //TODO: resize pic
 
 function MyPic() {
+  const [activate, setActivate] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => setActivate(true), 5300);
+    //return () => setActivate(false);
+  }, []);
   return (
     <figure className="MyPic">
       <Link to="/about">
         <img src={moi} alt="me" title="about me" />
       </Link>
+      <span className={`left ${activate ? "activate" : ""}`}></span>
+      <span className={`right ${activate ? "activate" : ""}`}></span>
+      {/* <span className={`left wrapper`}>
+        <span className="left-triangle"></span>
+      </span>
+      */}
     </figure>
   );
 }
 
 export default MyPic;
+
+//67.55, 67.4
