a610e27fe0b7ec5885d194e53ce7b3a19443fd3b
Hugo Carvalho <webnoico1978@gmail.com>
2024-10-01 18:54:02 +0200

    added remaining icons to skills

--- src/Pages/Skills/RenderSkills.js
+++ src/Pages/Skills/RenderSkills.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { IsActiveContext } from "../../components/Header/NavButtons/RenderNavButtons";
+import FilterBar from "../Projects/FilterBy/FilterBy";
 import "./RenderSkills.scss";
-import SingleSkill, { RelatedSkills } from "./SkillCards";
+import { RelatedSkills, SingleSkill, SingleSkillSvg } from "./SkillCards";
+import datadog from "./assets/datadog.svg";
+import mui from "./assets/material-ui.svg";
+import prisma from "./assets/prisma-3.svg";
+import sentry from "./assets/sentry.svg";
+import supabase from "./assets/supabase.svg";
+
 
 function Skills() {
   const pageIsActive = React.useContext(IsActiveContext);
@@ -11,46 +18,56 @@ function Skills() {
   }, [pageIsActive]);
 
   return (
-    <div className="Skills">
-      <main>
-        <SingleSkill classes="bg-javascript" name="JavaScript"/>
-        <SingleSkill classes="bg-react" name="React"></SingleSkill>
-        <SingleSkill classes="bg-typescript" name="Typescript"/>
-        <SingleSkill classes="bg-react" name="React Query"></SingleSkill>
-        <SingleSkill classes="bg-html-5" name="Semantic HTML"></SingleSkill>
-        <SingleSkill classes="bg-styled-components" name="StyledComponents"/>
-        <RelatedSkills
-          skillOne="CSS"
-          classesOne="bg-css3"
-          skillTwo="Sass"
-          classesTwo="bg-sass"
-        />
-        <SingleSkill name="Material UI"/>
-        <SingleSkill classes="bg-jest" name="Jest"/>
-        <SingleSkill classes="bg-jest" name="Cypress"/>
-        <SingleSkill
-          classes="bg-testing_library"
-          name={<small>React Testing Library</small>}
-        />
-        <SingleSkill classes="bg-dynamoDB" name="DynamoDB"/>
-        <SingleSkill classes="bg-dynamoDB" name="Cognito"/>
-        <SingleSkill classes="bg-postgreSQL" name="PostgreSQL"></SingleSkill>
-        <SingleSkill name="Supabase"/>
-        <SingleSkill classes="bg-redux" name="Redux"></SingleSkill>
-        <SingleSkill name="Datadog"/>
-        <SingleSkill name="Prisma"/>
+    <>
+      <FilterBar></FilterBar>
+        <div className="Skills">
+          <main>
+            <SingleSkill classes="bg-javascript" name="JavaScript"/>
+            <SingleSkill classes="bg-react" name="React"></SingleSkill>
+            <SingleSkill classes="bg-typescript" name="Typescript"/>
+            <SingleSkill classes="bg-react" name="React Query"></SingleSkill>
+            <SingleSkill classes="bg-html-5" name="Semantic HTML"></SingleSkill>
+            <SingleSkill classes="bg-styled-components" name="StyledComponents"/>
+            <RelatedSkills
+              skillOne="CSS"
+              classesOne="bg-css3"
+              skillTwo="Sass"
+              classesTwo="bg-sass"
+            />
+            {/* <SingleSkill name="Material UI"/> */}
+            <SingleSkillSvg src={mui} name={"Material UI"}/>
+            <SingleSkill classes="bg-jest" name="Jest"/>
+            <SingleSkill classes="bg-jest" name="Cypress"/>
+            <SingleSkill
+              classes="bg-testing_library"
+              name={<small>React Testing Library</small>}
+            />
+            <SingleSkill classes="bg-dynamoDB" name="DynamoDB"/>
+            <SingleSkill classes="bg-dynamoDB" name="Cognito"/>
+            <SingleSkill classes="bg-postgreSQL" name="PostgreSQL"></SingleSkill>
+            {/* <SingleSkill name="Supabase"/> */}
+            <SingleSkillSvg src={supabase} style={{height: "40%"}}/>
+            <SingleSkill classes="bg-redux" name="Redux"></SingleSkill>
+            {/* <SingleSkill name="Sentry"/> */}
+            <SingleSkillSvg src={sentry} name={"Sentry"}/>
+            {/* <SingleSkill name="Datadog"/> */}
+            <SingleSkillSvg src={datadog} name={"Datadog"}/>
+            {/* <SingleSkill name="Prisma"/> */}
+            <SingleSkillSvg src={prisma} name={"Prisma"}/>
 
 
-        <RelatedSkills
-          skillOne="Git"
-          classesOne="bg-git"
-          skillTwo="GitHub"
-          classesTwo="bg-github special"
-        />
 
-        {/* New Skill */}
-      </main>
+            <RelatedSkills
+              skillOne="Git"
+              classesOne="bg-git"
+              skillTwo="GitHub"
+              classesTwo="bg-github special"
+            />
+
+            {/* New Skill */}
+          </main>
     </div>
+    </>
   );
 }
 
--- src/Pages/Skills/SkillCards.js
+++ src/Pages/Skills/SkillCards.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./SkillCards.scss";
 
-function SingleSkill({classes, name}) {
+export function SingleSkill({classes, name}) {
   return (
     <div className="container__skill-card">
       <div className="wire">
@@ -35,4 +35,19 @@ export function RelatedSkills(props) {
   );
 }
 
-export default SingleSkill;
+export const SingleSkillSvg = ({src, name, style}) => {
+  return (
+    <div className="container__skill-card">
+  <div className="wire">
+  <span className="nail"></span>
+  </div>
+    <div className="skill-box center">
+      <div style={{height: "50%", ...style}} >
+        {<img src={src} width={"100%"} height={"100%"} alt={`${name} icon`}/>}
+      </div>
+      {name && <span>{name}</span>}
+  </div>
+  </div>
+
+  )
+}
