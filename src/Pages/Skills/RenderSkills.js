603d2d5d9430e10bc9fa234d09a7c7b895cdc662
Hugo Carvalho <webnoico1978@gmail.com>
2024-10-01 21:25:07 +0200

    .

--- src/Pages/Skills/RenderSkills.js
+++ src/Pages/Skills/RenderSkills.js
@@ -1,14 +1,9 @@
 import React from "react";
 import { IsActiveContext } from "../../components/Header/NavButtons/RenderNavButtons";
+import { SKILLS_DATA } from "../../config/Skills";
 import FilterBar from "../Projects/FilterBy/FilterBy";
 import "./RenderSkills.scss";
 import { RelatedSkills, SingleSkill, SingleSkillSvg } from "./SkillCards";
-import datadog from "./assets/datadog.svg";
-import mui from "./assets/material-ui.svg";
-import prisma from "./assets/prisma-3.svg";
-import sentry from "./assets/sentry.svg";
-import supabase from "./assets/supabase.svg";
-
 
 function Skills() {
   const pageIsActive = React.useContext(IsActiveContext);
@@ -22,8 +17,32 @@ function Skills() {
       <FilterBar></FilterBar>
         <div className="Skills">
           <main>
-            <SingleSkill classes="bg-javascript" name="JavaScript"/>
-            <SingleSkill classes="bg-react" name="React"></SingleSkill>
+            {SKILLS_DATA.map(skill => {
+              if(skill.name === "React Testing Library"){
+                return <SingleSkill classes={skill.class} name={<small>React Testing Library</small>}/>
+              }
+              if(skill.name === "Supabase"){
+                return <SingleSkillSvg src={skill.src} style={{height: "40%"}}/>
+              }
+              if(skill.type === "single"){
+                return <SingleSkill classes={skill.class} name={skill.name}/>
+              }
+              if (skill.type === "singleSvg"){
+                return <SingleSkillSvg src={skill.src} name={skill.name}/>
+              }
+              if(skill.type === "dual"){
+                console.log(skill.name)
+                return <RelatedSkills
+                skillOne={skill.name.one}
+                classesOne={skill.class.one}
+                skillTwo={skill.name.two}
+                classesTwo={skill.class.two}
+              />
+              }
+              throw new Error(`skill is not mapped!${skill}`)
+            })}
+
+            {/* <SingleSkill classes="bg-react" name="React"></SingleSkill>
             <SingleSkill classes="bg-typescript" name="Typescript"/>
             <SingleSkill classes="bg-react" name="React Query"></SingleSkill>
             <SingleSkill classes="bg-html-5" name="Semantic HTML"></SingleSkill>
@@ -34,7 +53,6 @@ function Skills() {
               skillTwo="Sass"
               classesTwo="bg-sass"
             />
-            {/* <SingleSkill name="Material UI"/> */}
             <SingleSkillSvg src={mui} name={"Material UI"}/>
             <SingleSkill classes="bg-jest" name="Jest"/>
             <SingleSkill classes="bg-jest" name="Cypress"/>
@@ -45,24 +63,18 @@ function Skills() {
             <SingleSkill classes="bg-dynamoDB" name="DynamoDB"/>
             <SingleSkill classes="bg-dynamoDB" name="Cognito"/>
             <SingleSkill classes="bg-postgreSQL" name="PostgreSQL"></SingleSkill>
-            {/* <SingleSkill name="Supabase"/> */}
             <SingleSkillSvg src={supabase} style={{height: "40%"}}/>
             <SingleSkill classes="bg-redux" name="Redux"></SingleSkill>
-            {/* <SingleSkill name="Sentry"/> */}
             <SingleSkillSvg src={sentry} name={"Sentry"}/>
-            {/* <SingleSkill name="Datadog"/> */}
             <SingleSkillSvg src={datadog} name={"Datadog"}/>
-            {/* <SingleSkill name="Prisma"/> */}
             <SingleSkillSvg src={prisma} name={"Prisma"}/>
-
-
 
             <RelatedSkills
               skillOne="Git"
               classesOne="bg-git"
               skillTwo="GitHub"
               classesTwo="bg-github special"
-            />
+            /> */}
 
             {/* New Skill */}
           </main>
--- src/config/Skills.js
+++ src/config/Skills.js
@@ -1,8 +1,48 @@
+import datadog from "../Pages/Skills/assets/datadog.svg"
+import mui from "../Pages/Skills/assets/material-ui.svg"
+import prisma from "../Pages/Skills/assets/prisma-3.svg"
+import sentry from "../Pages/Skills/assets/sentry.svg"
+import supabase from "../Pages/Skills/assets/supabase.svg"
 const languages = ["React", "JavaScript", "Typescript", "HTML"]
 const styles = ["CSS", "StyledComponents", "Material UI"]
 const testing = ["Jest", "Cypress", "React Testing Library"]
 const dbRelated = ["React Query", "AWS Cognito", "AWS DynamoDb", "PostgreSQL", "Prisma"]
 const monitoring = ["Sentry", "Datadog"]
 const others = ["Jira"]
-
 export const erblotseSkills = [...languages, ...styles, ...testing, ...dbRelated, ...monitoring, ...others]
+
+const SKILL_CATEGORIES = {
+  styling: "styling",
+  testing: "testing",
+  backend: "backend",
+  monitoring: "monitoring",
+  versioning: "versioning",
+  reactEcosystem: "reactEcosystem"
+}
+
+//DB & DB tools
+//Backend
+//React Ecosystem
+
+export const SKILLS_DATA = [
+  { name: "JavaScript", category: SKILL_CATEGORIES.reactEcosystem, class: "bg-javascript", type: "single" },
+  { name: "React", category: SKILL_CATEGORIES.reactEcosystem, class: "bg-react", type: "single" },
+  { name: "Typescript", category: SKILL_CATEGORIES.reactEcosystem, class: "bg-typescript", type: "single" },
+  { name: "React Query", category: SKILL_CATEGORIES.reactEcosystem, class: "bg-react", type: "single" },
+  { name: "Semantic HTML", category: SKILL_CATEGORIES.reactEcosystem, class: "bg-html-5", type: "single" },
+  { name: "StyledComponents", category: SKILL_CATEGORIES.styling, class: "bg-styled-components", type: "single" },
+  { name: {one: "CSS", two: "Sass"}, category: SKILL_CATEGORIES.styling, class: {one: "bg-css3", two: "bg-sass"}, type: "dual"},
+  { name: "Material UI", category: SKILL_CATEGORIES.styling, class: "bg-mui", type: "singleSvg", src: mui},
+  { name: "Jest", category: SKILL_CATEGORIES.testing, class: "bg-jest", type: "single" },
+  { name: "Cypress", category: SKILL_CATEGORIES.testing, class: "bg-jest", type: "single" },
+  { name: "React Testing Library", category: SKILL_CATEGORIES.testing, class: "bg-testing_library", type: "single" },
+  { name: "DynamoDB", category: SKILL_CATEGORIES.backend, class: "bg-dynamoDB", type: "single" },
+  { name: "Cognito", category: SKILL_CATEGORIES.backend, class: "bg-dynamoDB", type: "single" },
+  { name: "PostgreSQL", category: SKILL_CATEGORIES.backend, class: "bg-postgreSQL", type: "single" },
+  { name: "Supabase", category: SKILL_CATEGORIES.backend, type: "singleSvg", src: supabase},
+  { name: "Redux", category: SKILL_CATEGORIES.reactEcosystem, class: "bg-redux", type: "single" },
+  { name: "Sentry", category: SKILL_CATEGORIES.monitoring, type: "singleSvg", src: sentry },
+  { name: "Datadog", category: SKILL_CATEGORIES.monitoring, type: "singleSvg", src: datadog },
+  { name: "Prisma", category: SKILL_CATEGORIES.reactEcosystem, type: "singleSvg", src: prisma },
+  { name: {one: "GitHub", two:"Git"}, category: SKILL_CATEGORIES.versioning, class: {one: "bg-github special", two:"bg-git" }, type: "dual" },
+];
