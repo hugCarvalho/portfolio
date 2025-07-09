e8ffc21f9fc3f2368168a0bd14e3a5b0f937b15f
Hugo Carvalho <webnoico1978@gmail.com>
2025-07-09 12:14:26 +0200

    .

--- src/Pages/About/RenderExperience.jsx
+++ src/Pages/About/RenderExperience.jsx
@@ -93,15 +93,11 @@ export const RenderExperienceEntries = () => {
 
     </div>
     <DatumSection>
-      <Datum>06.2022-04.2025:</Datum>
+      <Datum>02.2021-04.2025:</Datum>
         <JobTitle>
           {isLanguageEnglish ? " Software Developer" : " Software Entwickler"}
         </JobTitle>
         <br/>
-      <Datum>02.2021-05.2022:</Datum>
-        <JobTitle>
-          {isLanguageEnglish ? " Junior Software Developer" : " Junior Software Entwickler"}
-        </JobTitle>
     </DatumSection>
     <DescriptionSection>
       <p>{isLanguageEnglish ? "Accomplishments:" : "Leistungen:"}</p>
